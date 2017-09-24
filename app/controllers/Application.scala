a9ec9cdaa3963822817899d0de20d995d209cdc9
Ilya Zinkovich <ilya.zinkovich@gmail.com>
2017-09-24 16:21:35 +0300

    Added websockets

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,61 +1,18 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
-import play.api.libs.oauth.{ConsumerKey, OAuthCalculator, RequestToken}
+import actors.TwitterStreamer
 import play.api.Play.current
-
-import scala.concurrent.Future
-import play.api.libs.concurrent.Execution.Implicits._
-import play.api.libs.iteratee.{Concurrent, Enumeratee, Enumerator, Iteratee}
-import play.api.libs.ws.WS
 import play.api.libs.json._
-import play.extras.iteratees._
+import play.api.mvc._
 
 class Application extends Controller {
 
-  def index = Action {
+  def index = Action { implicit request =>
     Ok(views.html.index("Your new application is ready."))
   }
 
-  def tweets = Action.async {
-    credentials.map { case (consumerKey, requestToken) =>
-      val (iteratee, enumerator) = Concurrent.joined[Array[Byte]]
 
-      val jsonStream: Enumerator[JsObject] =
-        enumerator &>
-          Encoding.decode() &>
-          Enumeratee.grouped(JsonIteratees.jsSimpleObject)
-
-      val loggingIteratee = Iteratee.foreach[JsObject] { value =>
-        Logger.info(value.toString)
-      }
-
-      jsonStream run loggingIteratee
-
-      WS
-        .url("https://stream.twitter.com/1.1/statuses/filter.json")
-        .sign(OAuthCalculator(consumerKey, requestToken))
-        .withQueryString("track" -> "cat")
-        .get { response =>
-          Logger.info("Status: " + response.status)
-          iteratee
-        }
-        .map { _ =>
-          Ok("Stream closed")
-        }
-    } getOrElse {
-      Future.successful(InternalServerError("Twitter credentials missing"))
-    }
+  def tweets = WebSocket.acceptWithActor[String, JsValue] {
+    request => out => TwitterStreamer.props(out)
   }
-
-  private def credentials: Option[(ConsumerKey, RequestToken)] = for {
-    apiKey <- Play.configuration.getString("twitter.apiKey")
-    apiSecret <- Play.configuration.getString("twitter.apiSecret")
-    token <- Play.configuration.getString("twitter.token")
-    tokenSecret <- Play.configuration.getString("twitter.tokenSecret")
-  } yield (
-    ConsumerKey(apiKey, apiSecret),
-    RequestToken(token, tokenSecret)
-  )
 }
